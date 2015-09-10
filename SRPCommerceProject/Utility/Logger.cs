544e4b4661c2d2fa2853a0640f49893cde21581b
Alexandre Roba <alexandre.roba@arobanet.be>
2015-09-10 11:01:40 +0200

    Initial Commit

---
+++ SRPCommerceProject/Utility/Logger.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SRPCommerceProject.Utility
+{
+    public static class Logger
+    {
+        public static void Error(string message, Exception exception)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
